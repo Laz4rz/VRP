fd2eb30f29058f89a7a7bc16c39a34645d2aee3d
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2021-03-07 23:23:40 +0100

    Added markers, MORE FUNCTIONS

--- map/static/map/addPoint.js
+++ map/static/map/addPoint.js
@@ -1,22 +1,27 @@
-var temporaryCoordinates = ''
+var temporaryCoordinates = null
 var points = []
-var selfLocalizationCoordinates
+const myLatlng = { lat: 52.229676, lng: 21.012229 };
+var globmap = null
+var currentWindow = null
+
 
 function pointConfirm(variable) {
     var point = document.createElement('p')
     if (variable === undefined) {
         var myDiv = document.getElementById('points')
         var deleteButton = document.createElement('button')
+        var marker = addMarker()
         deleteButton.onclick = function () {
             point.remove()
+            marker.setMap(null)
+            marker.setPosition(null)
         }
         deleteButton.appendChild(document.createTextNode('x'))
         deleteButton.className = 'deleteButton'
         point.id = 'point' + points.length
         point.className = 'center'
         point.appendChild(deleteButton)
-        point.appendChild(document.createTextNode(temporaryCoordinates))
-        addMarker()
+        point.appendChild(document.createTextNode(temporaryCoordinates.lat().toString() + temporaryCoordinates.lng().toString()))
     }
     else{
         var myDiv = document.getElementById('selfLocalization')
@@ -30,40 +35,31 @@ function pointConfirm(variable) {
     myDiv.appendChild(point)
 }
 
-// function buttonCreate(){
-//     var myDiv = document.getElementById('dodaj')
-//     var pointConfirmButton = document.createElement("button")
-//     pointConfirmButton.id = 'pointConfirm'
-//     pointConfirmButton.appendChild(document.createTextNode('Potwierdź'))
-//     myDiv.appendChild(pointConfirmButton)
-//     alert(temporaryCoordinates)
-// }
 
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-        browserHasGeolocation ?
-            "Error: The Geolocation service failed." :
-            "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-}
-
-function addMarker(map){
-    new google.maps.Marker({
+function addMarker(){
+    const svgMarker = {
+        path:
+            "M 14 11 l 2 -4 l -8 0 l 4 8 z M 12 2.016 q 2.906 0 4.945 2.039 t 2.039 4.945 q 0 1.453 -0.727 3.328 t -1.758 3.516 t -2.039 3.07 t -1.711 2.273 l -0.75 0.797 q -0.281 -0.328 -0.75 -0.867 t -1.688 -2.156 t -2.133 -3.141 t -1.664 -3.445 t -0.75 -3.375 q 0 -2.906 2.039 -4.945 t 4.945 -2.039 z",
+        fillColor: "blue",
+        fillOpacity: 0.6,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(15, 30),
+    };
+    let newMarker = new google.maps.Marker({
         position: temporaryCoordinates,
-        map: map,
+        map: globmap,
+        icon: svgMarker,
     });
+    currentWindow.close();
+    return newMarker
 }
 
-function initMap() {
-    const myLatlng = { lat: 52.229676, lng: 21.012229 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
-        center: myLatlng,
-    });
 
+
+function infoWindowMechanism(map){
     //                                          INFO WINDOW
     //Create the initial InfoWindow.
     let infoWindow = new google.maps.InfoWindow({
@@ -71,8 +67,7 @@ function initMap() {
         position: myLatlng,
     });
     infoWindow.open(map);
-
-    // Configure the click listener.
+    //Configure the click listener.
     map.addListener("click", (mapsMouseEvent) => {
         // Close the current InfoWindow.
         infoWindow.close();
@@ -80,16 +75,20 @@ function initMap() {
         infoWindow = new google.maps.InfoWindow({
             position: mapsMouseEvent.latLng,
         });
-
-        temporaryCoordinates = [mapsMouseEvent.latLng.lat().toString(), mapsMouseEvent.latLng.lng().toString()]
-
+        //temporaryCoordinates = [mapsMouseEvent.latLng.lat().toString(), mapsMouseEvent.latLng.lng().toString()]
+        temporaryCoordinates = infoWindow.position
         infoWindow.setContent(
-            'Szerokość: ' + temporaryCoordinates[0] + '<br />' + 'Długość: ' + temporaryCoordinates[1] + '<br /><br />' + '<div class="center" id="dodaj"><button onclick="pointConfirm()">Potwierdź</button> </div>'
+            'Szerokość: ' + temporaryCoordinates.lat().toString() + '<br />' + 'Długość: ' + temporaryCoordinates.lng().toString() + '<br /><br />' + '<div class="center" id="dodaj"><button onclick="pointConfirm()">Potwierdź</button> </div>'
         );
-
         infoWindow.open(map);
+        currentWindow = infoWindow
     });
+}
 
+
+
+function selfLocalize(map){
+    let infoWindow = new google.maps.Marker()
     //                                 SELFLOCALIZE
     const locationButton = document.createElement("button");
     locationButton.textContent = "Zlokalizuj";
@@ -104,11 +103,23 @@ function initMap() {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude,
                     };
+                    const svgMarker = {
+                        path:
+                            "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                        fillColor: "green",
+                        fillOpacity: 0.6,
+                        strokeWeight: 0,
+                        rotation: 0,
+                        scale: 2,
+                        anchor: new google.maps.Point(15, 30),
+                    };
+                    infoWindow.setIcon(svgMarker)
                     infoWindow.setPosition(pos);
-                    infoWindow.setContent("Location found.");
-                    infoWindow.open(map);
+                    //infoWindow.setContent("Location found.");
+                    infoWindow.setMap(map)
+                    //infoWindow.open(map);
                     map.setCenter(pos);
-                    pointConfirm(pos)
+                    pointConfirm(pos, map)
                 },
                 () => {
                     handleLocationError(true, infoWindow, map.getCenter());
@@ -120,3 +131,27 @@ function initMap() {
         }
     });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation ?
+            "Error: The Geolocation service failed." :
+            "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
+
+
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: myLatlng,
+    });
+    globmap = map
+
+    infoWindowMechanism(map)
+    selfLocalize(map)
+}
+
